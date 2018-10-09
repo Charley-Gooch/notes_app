1597aa26d27fa5c93a664b8061f620ed1a1dbe18
Charley Gooch <chazgooch@gmail.com>
2018-10-09 09:21:18 +0000

    test made to check for initialize method

--- spec/Notes_Spec.rb
+++ spec/Notes_Spec.rb
@@ -1,4 +1,13 @@
 require 'Notes'
 
 describe Notes do
+ @note = nil
+ before do
+  @note = Notes.new()
+ end
+ describe '#initialize' do
+  it 'responds to initialize method call' do
+   expect(@note).to respond_to(:initialize)
+  end
+ end
 end
