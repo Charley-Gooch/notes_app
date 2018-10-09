400ded78249a406e16e30ca9359b09d669b8a680
Charley Gooch <chazgooch@gmail.com>
2018-10-09 10:58:36 +0000

    base layer testing created with full functionality implemented now

--- lib/Main.rb
+++ lib/Main.rb
@@ -1,28 +1,81 @@
-class Main
-  def main
-    optionSort(notesMenu())
-  end
+require_relative 'Notes'
 
-  def notesMenu
-    puts "Please choose an option by typing the number associated:"
-    puts "1) Create a new note"
-    puts "2) View created notes"
-    puts "3) Delete created notes"
-    gets.chomp.to_s
+@existingNotes = {}
+@count = 1
+@quit = false
+def main
+  @count += @existingNotes.length
+  while ( @quit == false ) do
+    selection = notesMenu()
+    optionSort(selection)
   end
+end
 
-  def optionSort(notesMenu)
-    case notesMenu
-    when "1"
-      puts "Please enter the title of your new note."
-      title = gets.chomp.downcase.capitalize
-      puts "Now, please enter the body of your new note."
-      body = gets.chomp.downcase.capitalize
-    when "2"
-      puts viewNotes().to_s
-    when "3"
-      puts "please enter the title of the note you wish to delete"
-      delete = gets.chomp.downcase.capitalize
+def notesMenu
+  puts "Please choose an option by typing the number associated:"
+  puts "1) Create a new note"
+  puts "2) View all created notes"
+  puts "3) View details of created note"
+  puts "4) Delete created notes"
+  puts "5) Quit"
+  puts ""
+  return gets.chomp.to_s
+end
+
+def optionSort(selection)
+  choice = selection
+  case choice
+  when "1"
+    puts "Please enter the title of your new note."
+    title = gets.chomp.downcase.capitalize
+    puts "Now, please enter the body of your new note."
+    body = gets.chomp.downcase.capitalize
+    @existingNotes[@count] = Notes.new(noteTitle: title, noteBody: body)
+    puts "Note created with ID: #{@count.to_s}"
+    puts ""
+  when "2"
+    viewAll()
+  when "3"
+    puts "Please enter the ID of the note you wish to view"
+    viewNote(gets.chomp.to_i)
+  when "4"
+    puts "Please enter the ID of the note you wish to delete"
+    deleteNote(gets.chomp.to_i)
+  when "5"
+    @quit = true
+  end
+end
+  
+def viewAll()
+@existingNotes.each do |x, y|
+    puts "Note: #{x}"
+    puts "Title: #{y.viewTitle}"
+    puts ""
+  end
+end
+
+def viewNote(id)
+  @existingNotes.each do |x, y|
+    if(x = id)
+      puts "Title: #{y.viewTitle()}"
+      puts "Body: #{y.viewBody()}"
+      puts ""
+    else
+      puts "Note not found"
     end
   end
 end
+
+def deleteNote(id)
+  @existingNotes.each do |x, y|
+    if(x = id)
+      @existingNotes.delete(x)
+      puts "Note #{x} has been deleted"
+      puts ""
+    else
+      puts "Note not found"
+    end
+  end
+end
+
+main()
--- spec/Main_Spec.rb
+++ spec/Main_Spec.rb
@@ -1,14 +1,30 @@
 require 'Main'
 
 describe Main do
-  @main = nil
-  before do
-    @main = Main.new
+  describe '#notesMenu' do
+    it 'responds to method to display inital menu' do
+      allow(subject).to receive(:selection) {"5"}
+      expect(subject).to respond_to(:notesMenu)
+    end
   end
-  it 'responds to method to display inital menu' do
-    expect(@main).to respond_to(:notesMenu)
+  describe '#optionSort' do
+    it 'responds to method that handles background processing' do
+      expect(subject).to respond_to(:optionSort)
+    end
   end
-  it 'responds to method that handles background processing' do
-    expect(@main).to respond_to(:optionSort)
+  describe '#viewNote' do
+    it 'responds to method that prints selected note to console' do
+      expect(subject).to respond_to(:viewNote)
+    end
+  end
+  describe '#viewAll' do
+    it 'responds to method that prints all created notes to console' do
+      expect(subject).to respond_to(:viewAll)
+    end
+  end
+  describe '#deleteNote' do
+    it 'responds to method that deletes selected note from saved notes' do
+      expect(subject).to respond_to(:deleteNote)
+    end
   end
 end
